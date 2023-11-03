7c071de55e082cc6a8acc0fc5b3ee3999a037f99
kjleonguerrero <139664760+kjleonguerrero@users.noreply.github.com>
2023-11-04 03:34:18 +1000

    added html for posts-new and posts-index

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,47 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <div>
+      <header>
+        <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
+      </header>
+
+      <div id="posts-new">
+        <h1>New post</h1>
+        <div>
+          TITLE: <input type="text" />
+          BODY: <input type="text" />
+          IMAGE: <input type="text" />
+        </div>
+      </div>
+
+      <div id="posts-index">
+        <h1>All posts</h1>
+      </div>
+      {/* BlogPost2 */}
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>BLOG POST 2</h2>
+        <p>Fish are friends not food!</p>
+        <img
+          src="https://scubadivingcostarica.com/wp-content/uploads/2015/03/fish-friends-not-food-large-msg-1131585409-2-300x225.jpg"
+          alt="Fish are friends not food"
+        />
       </div>
-      <h1>Hello World!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      {/* BlogPost1 */}
+      <div>
+        <h2>BLOG POST 1</h2>
+        <p>Dog is a mans bestfriend!</p>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL7JNQWMixefJ6knA8yFatxnl-fce8BvGNwOxmdoSw2f0cBN1oL704HGX_uLgIc7InNgo&usqp=CAU"
+          alt="Dog is a mans bestfriend"
+        />
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+
+      <footer>
+        <p>Copyright 20XX</p>
+      </footer>
+    </div>
   );
 }
 
